0e5105672f091708918c1f5e8c767c96be785713
张青松 <zhangqingsong@zhangqiongdeMBP.lan>
2024-10-03 12:04:13 +0800

    server build

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-import Game from './components/Game';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+import Game from "./components/Game";
 
-const socket = io('http://localhost:4000');
+const socket = io("https://tug-of-war-5s6f.vercel.app");
 
 function App() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const [side, setSide] = useState(null);
   const [gameStarted, setGameStarted] = useState(false);
 
   const joinGame = (selectedSide) => {
     if (username) {
       setSide(selectedSide);
-      socket.emit('join', { username, selectedSide });
+      socket.emit("join", { username, selectedSide });
     }
   };
 
   useEffect(() => {
-    socket.on('gameStart', () => setGameStarted(true));
+    socket.on("gameStart", () => setGameStarted(true));
   }, []);
 
   return (
     <div className="App">
       {!username ? (
-        <input 
-          type="text" 
-          placeholder="Enter username" 
-          onChange={(e) => setUsername(e.target.value)} 
+        <input
+          type="text"
+          placeholder="Enter username"
+          onChange={(e) => setUsername(e.target.value)}
         />
       ) : !gameStarted ? (
         <div>
-          <button onClick={() => joinGame('left')}>Left</button>
-          <button onClick={() => joinGame('right')}>Right</button>
+          <button onClick={() => joinGame("left")}>Left</button>
+          <button onClick={() => joinGame("right")}>Right</button>
         </div>
       ) : (
         <Game username={username} side={side} />
